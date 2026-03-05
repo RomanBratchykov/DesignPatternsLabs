caa5d346acea4803796b33dd612f10d4e6afa0d5
RomanBratchykov <romanbratchykov@gmail.com>
2026-03-05 12:17:24 +0200

    Completed singleton pattern

--- GOFPatterns/Lab1/Patterns/Singleton.cs
+++ GOFPatterns/Lab1/Patterns/Singleton.cs
@@ -7,9 +7,34 @@ namespace Lab1.Patterns
 {
     public class Singleton : IDemonstrate
     {
+        public Singleton(){}
+
         public void Demonstrate()
         {
-            Console.WriteLine("This is the Singleton pattern demonstration.");
+            DatabaseConnection db1 = DatabaseConnection.Instance;
+
+            DatabaseConnection db2 = DatabaseConnection.Instance;
+
+            if (ReferenceEquals(db1, db2))
+            {
+                Console.WriteLine("Success: Both variables contain the exact same instance, thus connection is shared.");
+            }        }
+    }
+    public sealed class DatabaseConnection
+    {
+        private static readonly Lazy<DatabaseConnection> _instance = 
+            new Lazy<DatabaseConnection>(() => new DatabaseConnection());
+
+        private DatabaseConnection()
+        {
+            Console.WriteLine("Connecting to database");
+        }
+
+        public static DatabaseConnection Instance => _instance.Value;
+
+        public void QueryData(string componentName)
+        {
+            Console.WriteLine($"      -> {componentName} is getting data from the shared Singleton connection.");
         }
     }
 }
--- GOFPatterns/Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ GOFPatterns/Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db45a524bd98639fd8cd512376eb82ba4a7c5a4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0dd1bea9218664fdd32a4891f64b17af61308d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
